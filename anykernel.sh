d96c7b1435a7838b84414a40399258143e238daf
Andy | アンディ <54360298+nullptr03@users.noreply.github.com>
2024-08-27 20:22:56 +0800

    update

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SingkoKernel by Tahu Gehot | SKG
+kernel.string=Greenforce Project build by SingkoLab
 do.devicecheck=1
 do.modules=0
 do.systemless=1
