fae21b8265e4d322c6f7cdf22756c2aab0e8374d
nullptr03 <nullptr03@singkolab.my.id>
2025-07-20 13:14:18 +0000

    Revert "Check flash dtb script"
    This reverts commit c3b0b56c88a75c7f0ff174bf8b79dc511f0e2fda.

--- anykernel.sh
+++ anykernel.sh
@@ -10,10 +10,7 @@ device.name1=moonstone
 device.name2=sunstone
 device.name3=stone
 do.cleanup=1
-'; } 
-
-kernel_flash_dtb=no
-# end properties
+'; } # end properties
 
 ### AnyKernel install
 # boot shell variables
@@ -29,10 +26,10 @@ split_boot;
 flash_boot;
 ## end boot install
 
-if [ "$kernel_flash_dtb" = "yes" ]; then
 # dtbo install
 flash_dtbo
 ## end of dtbo install
+
 # vendor_boot shell variables
 block=vendor_boot;
 
@@ -43,4 +40,3 @@ reset_ak;
 split_boot;
 flash_boot;
 ## end vendor_boot install
-fi
