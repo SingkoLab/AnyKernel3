26a18e6aa2a7b4cbbad9669dca463e09d71abef4
nullptr03 <nullptr03@singkolab.my.id>
2026-03-31 21:10:19 +0800

    get rid of dtb and dtbo logic causing serious issues

--- anykernel.sh
+++ anykernel.sh
@@ -25,18 +25,3 @@ no_block_display=1;
 split_boot;
 flash_boot;
 ## end boot install
-
-# dtb install
-# auto check the dtb & dtbo file so
-# we don't need another check
-if [ -f dtbo ]; then
-  flash_dtbo
-elif [ -f dtb ]; then
-  block=vendor_boot;
-  reset_ak;
-  split_boot;
-  flash_boot;
-else
-  ui_print " " "No dtb or dtbo file found. Skipping."
-fi
-## end dtb install
