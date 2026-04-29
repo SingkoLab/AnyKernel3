8d074c8bb295a49ca21d913b0d30771d50290e1d
nullptr03 <nullptr03@singkolab.my.id>
2026-04-29 18:10:30 +0800

    tools: Import ak3-custom script

--- anykernel.sh
+++ anykernel.sh
@@ -10,16 +10,51 @@ do.cleanup=1
 device.name1=moonstone
 device.name2=sunstone
 device.name3=stone
+device.name4=gemstone
 '; } # end properties
 
 # boot shell variables
 BLOCK=boot;
 IS_SLOT_DEVICE=auto;
+NO_BLOCK_DISPLAY=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
+. tools/ak3-custom.sh;
 
+ui_print "   _____  _______  _  _  _  _____  _______ "
+ui_print "  / ____||  ___  || |/ \| ||_   _||__   __|"
+ui_print "  \____ \| |___| ||  / \  |  | |     | |   "
+ui_print "  ____/ /|  ___  || /   \ | _| |_    | |   "
+ui_print " |_____/ |_|   |_||_|   |_||_____|   |_|   "
+ui_print "-------------------------------------------"
+ui_print "        pokok e joget pokok e joget        "
+ui_print "-------------------------------------------"
+
+ui_print " "
+
+ui_print "Patching sawit boot partition..."
 # boot install
 split_boot;
 flash_boot;
 ## end boot install
+
+ui_print "Patching sawit dtbo partition..."
+# dtbo install
+erase_dtbo
+## end dtbo install
+
+ui_print "Patching sawit vendor_boot partition..."
+# vendor_boot shell variables
+BLOCK=vendor_boot;
+
+# reset for vendor_boot patching
+reset_ak;
+
+# vendor_boot install
+split_boot;
+check_vendor_hals;
+flash_boot;
+## end vendor_boot install
+
+ui_print "Sawit installed, enjoy the Sawit feels."
