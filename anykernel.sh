d6469d00a119c25cf84ef7a3f40b7c5e5a732dd9
nullptr03 <nullptr03@singkolab.my.id>
2026-07-18 19:14:54 +0800

    Update anykernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,35 +4,35 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=SingkoKernel by @nullptr03v2
-do.devicecheck=1
-device.name1=taoyao
+kernel.string=
+do.devicecheck=0
 do.cleanup=1
 '; } # end properties
 
-### AnyKernel install
 # boot shell variables
-block=boot;
-is_slot_device=auto;
-no_block_display=1;
+BLOCK=boot;
+IS_SLOT_DEVICE=auto;
+NO_BLOCK_DISPLAY=1;
+RAMDISK_COMPRESSION=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+ui_print "Ximi is bad at OSS, hope this run perfectly without Ximi cancer."
+
 # boot install
 split_boot;
 flash_boot;
-#flash_dtbo;
+flash_dtbo;
 ## end boot install
 
 # vendor_boot shell variables
-#block=vendor_boot;
-#is_slot_device=auto;
+BLOCK=vendor_boot;
 
 # reset for vendor_boot patching
-#reset_ak;
+reset_ak;
 
 # vendor_boot install
-#split_boot;
-#flash_boot;
+split_boot;
+flash_boot;
 ## end vendor_boot install
