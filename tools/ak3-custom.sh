14cac5956f68b442e80f862b153fa4b935fdc7c3
nullptr03 <nullptr03@singkolab.my.id>
2026-07-21 17:27:24 +0800

    correct patch cmdline

--- tools/ak3-custom.sh
+++ tools/ak3-custom.sh
@@ -20,10 +20,10 @@ inject_cmdline() {
 
   if [ "$vendor_mounted" -eq 1 ]; then
     if grep -qs displayfeature /vendor/bin/hw/vendor.qti.hardware.display.composer-service; then
-      patch_cmdline kernel.xiaomi_displayfeature kernel.xiaomi_displayfeature=1
+      patch_cmdline xiaomi.displayfeature xiaomi.displayfeature
     fi;
     if [ -e /vendor/lib64/hw/consumerir.default.so -o -e /vendor/lib64/hw/consumerir.lahaina.so ]; then
-      patch_cmdline kernel.xiaomi_ir_spi kernel.xiaomi_ir_spi=1
+      patch_cmdline xiaomi.ir_spi xiaomi.ir_spi
     fi;
   fi;
 }
